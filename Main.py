fb0d3d905638a8538839c856f5fd58f0631f206e
Haniff <kamalhaniff5@gmail.com>
2025-06-22 15:57:54 +0800

    Initial commit

---
+++ Main.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/system/modeling')
+def modeling():
+    return render_template('modeling system.html')
+
+@app.route('/system/analysis')
+def analysis():
+    return render_template('system analysis.html')
+
+@app.route('/control/pid')
+def pid():
+    return render_template('PID.html')
+
+@app.route('/control/state-space')
+def statespace():
+    return render_template('State Space.html')
+
+@app.route('/simulink/modeling')
+def Smodeling():
+    return render_template('Simulink Modeling.html')
+
+@app.route('/simulink/control')
+def control():
+    return render_template('control.html')
+
+@app.route('/simulink/simscape')
+def simscape():
+    return render_template('simscape.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
